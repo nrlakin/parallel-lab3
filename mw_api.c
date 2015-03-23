76f09a3f33a3733b45426c7ff6991a3d2e22f65b
Jonathan Huang <jon.hc.huang@gmail.com>
2015-03-23 18:43:59 -0400

    Master state checkpointing

--- mw_api.c
+++ mw_api.c
@@ -47,18 +47,18 @@ struct my_struct *ids = NULL;
 
 void add_id(unsigned long input_id, int count) {
   struct my_struct *s;
-  HASH_FIND(ids, &input_id, s);  /* id already in the hash? */
+  HASH_FIND(hh, ids, &input_id, sizeof(unsigned long), s);  /* id already in the hash? */
   if (s==NULL) {
     s = (struct my_struct*)malloc(sizeof(struct my_struct));
     s->id = input_id;
-    HASH_ADD( ids, id, s );  /* id: count of key field */
+    HASH_ADD(hh, ids, id, sizeof(unsigned long), s);  /* id: count of key field */
   }
-  strcpy(s->count, count);
+  s->count = count;
 }
 
 struct my_struct *find_id(unsigned long input_id) {
   struct my_struct *s;
-  HASH_FIND( ids, &input_id, s );  /* s: output pointer */
+  HASH_FIND(hh, ids, &input_id, sizeof(unsigned long), s);  /* s: output pointer */
   return s;
 }
 
@@ -169,12 +169,12 @@ void WriteJob(FILE * jf_stream, job_data_t *job_ptr, struct mw_api_spec *f) {
     fprintf(stderr, "Error writing to job file.\n");
     exit(1);
   }
-  printf("Serializing job_id %ld\n", job_ptr->job_id);
+  // printf("Serializing job_id %ld\n", job_ptr->job_id);
   fwrite(&(job_ptr->job_id), 1, sizeof(unsigned long), jf_stream);
   if (f->serialize_work2(job_ptr->work_ptr, &byte_stream, &length) == 0) {
     fprintf(stdout, "Error serializing work on master process.\n");
   }
-  printf("serialized job\n");
+  // printf("serialized job\n");
   fwrite(&length, 1, sizeof(int), jf_stream);
   fwrite(byte_stream, 1, length, jf_stream);
   free(byte_stream);
@@ -189,15 +189,15 @@ long int ReadJob(FILE * jf_stream, long int offset, job_data_t **new_job, struct
   fseek(jf_stream, offset, SEEK_SET);
   bytes_read += fread(&temp_job_id, 1, sizeof(unsigned long), jf_stream);
   if (bytes_read == 0)return 0;
-  printf("mallocing job node.\n");
+  // printf("mallocing job node.\n");
   if (NULL == (*new_job = (job_data_t*) malloc(sizeof(job_data_t)))) {
     fprintf(stderr, "malloc failed initializing job queue...\n");
     return 0;
   };
   (*new_job)->job_id = temp_job_id;
   bytes_read += fread(&work_size, 1, sizeof(int), jf_stream);
-  printf("reading file, mallocing byte stream\n");
-  printf("reading %d bytes.\n", work_size);
+  // printf("reading file, mallocing byte stream\n");
+  // printf("reading %d bytes.\n", work_size);
   if (NULL == (byte_stream = (unsigned char*) malloc(sizeof(unsigned char)*work_size))) {
     fprintf(stderr, "malloc failed allocating byte stream...\n");
     return 0;
@@ -207,23 +207,23 @@ long int ReadJob(FILE * jf_stream, long int offset, job_data_t **new_job, struct
   free(byte_stream);
   (*new_job)->result_ptr = NULL;
   (*new_job)->next_job = NULL;
-  printf("bytes read: %ld\n", bytes_read);
+  // printf("bytes read: %ld\n", bytes_read);
   return bytes_read;
 }
 
 void WriteResult(FILE * jf_stream, job_data_t *job_ptr, unsigned char *byte_stream, int length) {
-  // FILE * jf_stream;
-  // NEED TO OVERWRITE FIRST RUN
-  // jf_stream = fopen("results.txt", "ab");
-
   if (jf_stream == NULL) {
     fprintf(stderr, "Error writing to job file.\n");
     exit(1);
   }
+  int test;
+  // printf("%lu\n", job_ptr->job_id);
   fwrite(&(job_ptr->job_id), 1, sizeof(unsigned long), jf_stream);
+  // printf("%d\n", length);
   fwrite(&length, 1, sizeof(int), jf_stream);
+  // printf("%s\n", byte_stream);
   fwrite(byte_stream, 1, length, jf_stream);
-  // fclose(jf_stream);
+
   return;
 }
 
@@ -235,15 +235,15 @@ long int ReadResult(FILE * jf_stream, long int offset, job_data_t **new_job, str
   fseek(jf_stream, offset, SEEK_SET);
   bytes_read += fread(&temp_job_id, 1, sizeof(unsigned long), jf_stream);
   if (bytes_read == 0) return 0;
-  printf("mallocing job node.\n");
+  // printf("mallocing job node.\n");
   if (NULL == (*new_job = (job_data_t*) malloc(sizeof(job_data_t)))) {
     fprintf(stderr, "malloc failed initializing job queue...\n");
     return 0;
   };
   (*new_job)->job_id = temp_job_id;
   bytes_read += fread(&result_size, 1, sizeof(int), jf_stream);
-  printf("reading file, mallocing byte stream\n");
-  printf("reading %d bytes.\n", result_size);
+  // printf("reading file, mallocing byte stream\n");
+  // printf("reading %d bytes.\n", result_size);
   if (NULL == (byte_stream = (unsigned char*) malloc(sizeof(unsigned char)*result_size))) {
     fprintf(stderr, "malloc failed allocating byte stream...\n");
     return 0;
@@ -253,40 +253,40 @@ long int ReadResult(FILE * jf_stream, long int offset, job_data_t **new_job, str
   (*new_job)->work_ptr = NULL;
   (*new_job)->next_job = NULL;
   free(byte_stream);
-  printf("bytes read: %ld\n", bytes_read);
+  // printf("bytes read: %ld\n", bytes_read);
   return bytes_read;
 }
 
-void RebuildQueues(job_queue_t *pendingPtr, job_queue_t *resultsPtr, struct mw_api_spec *f) {
+int RebuildQueues(job_queue_t *pendingPtr, job_queue_t *resultPtr, struct mw_api_spec *f) {
   long int bytes_read, offset = 0;
   FILE * jf_stream;
-  printf("rebuilding done queue...\n");
+  // printf("rebuilding done queue...\n");
   jf_stream = fopen("results.txt", "rb");
-  printf("opened file.\n");
+  // printf("opened file.\n");
   job_data_t *new_job;
   do {
     bytes_read = ReadResult(jf_stream, offset, &new_job, f);
     if (bytes_read != 0) {
-      printf("adding to queue.\n");
+      // printf("adding to queue.\n");
       enqueue(resultPtr, new_job);
-      printf("adding to hash.\n");
+      // printf("adding to hash %lu\n", new_job->job_id);
       add_id(new_job->job_id, 1);
     }
     offset += bytes_read;
   } while(bytes_read != 0);
   fclose(jf_stream);
 
-  printf("rebuilding pending queue...\n");
+  // printf("rebuilding pending queue...\n");
   jf_stream = fopen("pool.txt", "rb");
-  printf("opened file.\n");
-  job_data_t *new_job;
+  // printf("opened file.\n");
   offset = 0;
   bytes_read = 0;
   do {
     bytes_read = ReadJob(jf_stream, offset, &new_job, f);
     if (bytes_read != 0) {
-      printf("checking hash.\n");
-      if (find_id(new_job->job_id) != NULL) {
+      // printf("checking hash %lu\n", new_job->job_id);
+      if (find_id(new_job->job_id) == NULL) {
+        // printf("Not Found in Hash\n");
         enqueue(pendingPtr, new_job);
       }
     }
@@ -294,16 +294,16 @@ void RebuildQueues(job_queue_t *pendingPtr, job_queue_t *resultsPtr, struct mw_a
   } while(bytes_read != 0);
   fclose(jf_stream);
   delete_all();
-  return;
+  return 1;
 }
 
-void WriteAllJobs(job_data_t *job_list, struct mw_api_spec *f) {
+void WriteAllJobs(job_data_t *first_job, struct mw_api_spec *f) {
   FILE * job_record;
-  job_data_t *job_ptr = job_list;
+  job_data_t *temp = first_job;
   job_record = fopen("pool.txt", "wb");
-  while (job_ptr != NULL) {
-    WriteJob(job_record, job_ptr, f);
-    job_ptr = job_ptr->next_job;
+  while (temp != NULL) {
+    WriteJob(job_record, temp, f);
+    temp = temp->next_job;
   }
   fclose(job_record);
   return;
@@ -335,13 +335,12 @@ void SendResults(int dest, mw_result_t **first_result, int n_results, struct mw_
 }
 
 void MW_Run(int argc, char **argv, struct mw_api_spec *f) {
-  int rank, n_proc, i, length;
+  int rank, n_proc, i, length, recov;
   unsigned char *receive_buffer;
   MPI_Status status;
 
   // Seed random # generator for failure testing
   //srand(time(NULL));
-
 
   // Get environment variables.
   MPI_Comm_size (MPI_COMM_WORLD, &n_proc);
@@ -369,15 +368,23 @@ void MW_Run(int argc, char **argv, struct mw_api_spec *f) {
     double start, end;
     // start timer
     start = MPI_Wtime();
+    mw_work_t **work_queue = NULL;
 
+    // START Testing Recovery
     // Init work queues
-    mw_work_t **work_queue = f->create(argc, argv);
+    work_queue = f->create(argc, argv);
     //mw_work_t **next_job = work_queue;
     //mw_result_t *result_queue[MASTER_QUEUE_LENGTH];
     //mw_result_t **next_result = result_queue;
     //result_queue[0] = NULL;
 
     InitializeJobQueue(&PendingQueue, work_queue);
+    WriteAllJobs(PendingQueue.first, f);
+    // END Testing Recovery
+
+    // // recov keeps track of whether process has been recovered
+    // recov = 0;
+    // recov = RebuildQueues(&PendingQueue, &DoneQueue, f);
 
     // Worker status array.
     unsigned char worker_status[n_proc-1];
@@ -385,13 +392,24 @@ void MW_Run(int argc, char **argv, struct mw_api_spec *f) {
     // Initialize by giving tasks to all workers.
     for (i=1; i<n_proc; i++) {
       WorkerQPtrs[i-1] = &WorkerQueues[i-1];    //match ptrs
+      // printf("gets here\n");
+      // if (queueEmpty(&PendingQueue)) {
+      //   printf("queue empty\n");
+      //   continue;
+      // }
+      // printf("assigning job\n");
       assign_job(i, &PendingQueue, WorkerQPtrs[i-1], f);
+      // printf("assigned job\n");
       worker_status[i-1] = BUSY;
     }
 
+    FILE * result_jf_stream;
+    int write_count;
+    write_count = 0;
     // Loop; give new jobs to workers as they return answers.
     while(1) {
       MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      // printf("probed\n");
       MPI_Get_count(&status, MPI_UNSIGNED_CHAR, &length);
       source = status.MPI_SOURCE;
       //printf("Got result from process %d of length %d\n", source, length);
@@ -401,7 +419,18 @@ void MW_Run(int argc, char **argv, struct mw_api_spec *f) {
       };
       MPI_Recv(receive_buffer, length, MPI_UNSIGNED_CHAR, source, MPI_ANY_TAG, MPI_COMM_WORLD,
             &status);
+      // printf("received\n");
       temp_job = dequeue(WorkerQPtrs[source-1]);
+      // printf("got work\n");
+
+      char* mode = "ab";
+      if (write_count == 0 && recov == 0) mode = "wb";
+      result_jf_stream = fopen("results.txt", mode);
+      write_count++;
+      // printf("Writing Results\n");
+      WriteResult(result_jf_stream, temp_job, receive_buffer, length);
+      fclose(result_jf_stream);
+
       if (temp_job != NULL) {
         //printf("deserializing job %ld.\n", temp_job->job_id);
         if (f->deserialize_results2(&(temp_job->result_ptr), receive_buffer, length) == 0) {
@@ -463,11 +492,13 @@ void MW_Run(int argc, char **argv, struct mw_api_spec *f) {
     printf("%f seconds elapsed.\n", end-start);
 
     // Clean up master data structures.
-    if (f->cleanup(work_queue, result_queue)) {
-      fprintf(stderr, "Successfully cleaned up memory.\n");
-    } else {
-      fprintf(stderr, "Error in user-defined clean function.\n");
-      return;
+    if (work_queue != NULL) {
+      if (f->cleanup(work_queue, result_queue)) {
+        fprintf(stderr, "Successfully cleaned up memory.\n");
+      } else {
+        fprintf(stderr, "Error in user-defined clean function.\n");
+        return;
+      }
     }
   } else {
     // Worker program.
