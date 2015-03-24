a94bdf955fb702c2e50afe571b18306a4fd5d62c
Jonathan Huang <jon.hc.huang@gmail.com>
2015-03-23 22:44:41 -0400

    Added atomic global message for arbitration

--- mw_comms.c
+++ mw_comms.c
@@ -4,6 +4,8 @@
 #include <mpi.h>
 #include "mw_comms.h"
 
+#define TAG_ARB     2
+
 int TO_Probe(int ms_timeout, int source, int tag, MPI_Comm comm, MPI_Status *status) {
   int flag = 0;
   double current, start = MPI_Wtime()*1000;
@@ -15,4 +17,11 @@ int TO_Probe(int ms_timeout, int source, int tag, MPI_Comm comm, MPI_Status *sta
     MPI_Iprobe(source, tag, comm, &flag, status);
   }
   return PROBE_SUCCESS;
+}
+
+void send_arb_all(int n_proc, int rank) {
+  int dummy, i;
+  for (i=1; i<n_proc && i != rank; i++) {
+    MPI_Send(&dummy, 0, MPI_INT, i, TAG_ARB, MPI_COMM_WORLD);
+  }
 }
